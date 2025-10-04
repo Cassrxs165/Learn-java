08f6ac8857072f62ae8859e46dd3d0b3801ed916
Cassrxs <hybridthry55@gmail.com>
2025-10-05 00:14:53 +0700

    coba" saja

---
+++ Soal_1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Soal_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("MASUKKAN ANGKA : ");
+
+        long original = sc.nextLong();
+        long n = Math.abs(original);
+        int sum = 0;
+        
+
+        while (n> 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+
+        boolean kelipatan3 = ( sum % 3 == 0);
+        boolean genap = (original % 2 == 0);
+
+        System.out.println("sum of digits: " + sum );
+        System.out.println("kelipatan 3: " + (kelipatan3 ? "YA" : "BUKAN" ));
+        System.out.println("genap: " + (genap ? "YA" : "BUKAN"));
+
+        sc.close();
+    }
+}
